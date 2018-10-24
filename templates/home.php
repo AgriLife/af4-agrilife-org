07571a0e53fa59c90894bc8d735083414ec0d461
Zachary Watkins <watkinza@gmail.com>
2018-10-24 10:04:35 -0500

    Improve layout through small to large breakpoints and reverse previous approach to fixing sub pixel rendering issue

--- templates/home.php
+++ templates/home.php
@@ -14,61 +14,39 @@ add_action('genesis_entry_content', function(){
 	?><div id="overview">
 		<div id="leaders-heading" class="layout-container"><h1><span class="heading-1">Leaders in </span><span class="heading-2">Agriculture,<br> Natural Resources,<br> &amp; Life Sciences</span></h1></div>
 		<div id="action-items" class="layout-container">
-			<div class="item item-1">
-				<div class="text-wrap-full">
-					<h2>Exceptional Items</h2>
-					<div class="description hide-for-small-only has-line">
-						<ul>
-							<li>Texas A&M AgriLife Extension</li>
-							<li>Texas A&M AgriLife Research</li>
-							<li>Texas A&M Veterinary Medical Diagnostic Laboratory</li>
-							<li>Texas A&M Forest Service</li>
-						</ul>
-					</div>
-				</div>
+			<div class="item item-1"><div class="center-y"><h2>Exceptional Items</h2>
+				<div class="description has-line hide-for-small-only"><ul><li>Texas A&M AgriLife Extension</li>
+				<li>Texas A&M AgriLife Research</li>
+				<li>Texas A&M Veterinary Medical Diagnostic Laboratory</li>
+				<li>Texas A&M Forest Service</li></ul></div></div>
 			</div>
-			<div class="item item-2">
-				<div class="wrap" style="background-image: url(<?php echo ALAF4_DIR_URL; ?>images/home-about.jpg);">
-					<div class="text-wrap">
-						<h2>What is <br>Texas A&M AgriLife?</h2>
-					</div>
-				</div>
-			</div>
-			<div class="item item-3 featured">
-				<div class="wrap" style="background-image: url(<?php echo ALAF4_DIR_URL; ?>images/home-impacts.jpg);">
-					<img src="<?php echo ALAF4_DIR_URL; ?>/images/home-impacts.jpg" alt="">
-					<div class="text-wrap">
-						<h2>Impacts</h2>
-						<div class="description hide-for-small-only has-line">Residents, AgriLife Extension, others work to 'Harvey-proof' Houston-area community</div>
-					</div>
-				</div>
-			</div>
-			<div class="item item-4">
-				<div class="wrap" style="background-image: url(<?php echo ALAF4_DIR_URL; ?>images/home-today.jpg);">
-					<div class="text-wrap">
-						<h2>AgriLife Today</h2>
-						<div class="description hide-for-small-only has-line">Texas A&M AgriLife to lead consortium in establishing Center</div>
-					</div>
-				</div>
-			</div>
-			<div class="item item-5"><div class="text-wrap-full"><h2>Vice Chancellor's <span class="big">Newsletter</span></h2><!-- Begin Mailchimp Signup Form -->
-				<div id="mc_embed_signup" class="description">
-					<form action="https://agrilife.us1.list-manage.com/subscribe/post?u=1a8a0ed97f45319b1319f2572&amp;id=449a7707a3" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-				    <div id="mc_embed_signup_scroll">
-							<div class="mc-field-group">
-								<label for="mce-EMAIL" class="screen-reader-text">Email Address </label>
-								<input type="email" value="" placeholder="Email" name="EMAIL" class="required email" id="mce-EMAIL">
-							</div>
-							<div id="mce-responses" class="clear">
-								<div class="response" id="mce-error-response" style="display:none"></div>
-								<div class="response" id="mce-success-response" style="display:none"></div>
-							</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-					    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_1a8a0ed97f45319b1319f2572_449a7707a3" tabindex="-1" value=""></div>
-					    <div class="clear"><input type="submit" value="Sign up" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-				    </div>
-					</form>
-				</div><script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';}(jQuery));var $mcj = jQuery.noConflict(true);</script><!--End mc_embed_signup-->
+			<div class="item item-2"><img class="hide-for-small-only" src="<?php echo ALAF4_DIR_URL; ?>/images/home-about.jpg" alt=""><h2>What is <br>Texas A&M AgriLife?</h2></div>
+			<div class="item  item-3 featured"><div class="wrap" style="background-image: url(<?php echo ALAF4_DIR_URL; ?>/images/home-impacts.jpg);"><img src="<?php echo ALAF4_DIR_URL; ?>/images/home-impacts.jpg" alt=""><h2>Impacts</h2>
+				<div class="description has-line hide-for-small-only">Residents, AgriLife Extension, others work to 'Harvey-proof' Houston-area community</div>
 			</div></div>
+			<div class="item item-4"><img class="hide-for-small-only" src="<?php echo ALAF4_DIR_URL; ?>/images/home-today.jpg" alt=""><h2>AgriLife Today</h2>
+				<div class="description has-line hide-for-small-only">Texas A&M AgriLife to lead consortium in establishing Center
+			</div></div>
+			<div class="item item-5">
+				<div class="center-y"><h2>Vice Chancellor's <span class="big">Newsletter</span></h2><!-- Begin Mailchimp Signup Form -->
+					<div id="mc_embed_signup" class="description">
+						<form action="https://agrilife.us1.list-manage.com/subscribe/post?u=1a8a0ed97f45319b1319f2572&amp;id=449a7707a3" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+					    <div id="mc_embed_signup_scroll">
+								<div class="mc-field-group">
+									<label for="mce-EMAIL" class="screen-reader-text">Email Address </label>
+									<input type="email" value="" placeholder="Email" name="EMAIL" class="required email" id="mce-EMAIL">
+								</div>
+								<div id="mce-responses" class="clear">
+									<div class="response" id="mce-error-response" style="display:none"></div>
+									<div class="response" id="mce-success-response" style="display:none"></div>
+								</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+						    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_1a8a0ed97f45319b1319f2572_449a7707a3" tabindex="-1" value=""></div>
+						    <div class="clear"><input type="submit" value="Sign up" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+					    </div>
+						</form>
+					</div><script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';}(jQuery));var $mcj = jQuery.noConflict(true);</script><!--End mc_embed_signup-->
+				</div>
+			</div>
 		</div>
 	</div>
 	<div id="agencies">
