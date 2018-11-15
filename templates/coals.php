5c4c208e7c7a33d896e733e989f26449048d7446
Zachary Watkins <watkinza@gmail.com>
2018-11-15 16:00:25 -0600

    Update template files

--- templates/coals.php
+++ templates/coals.php
@@ -9,21 +9,21 @@ add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_c
 
 add_action('genesis_entry_content', function(){
 	?><div class="content-heading-image"><img class="background show-for-small-only" src="<?php echo ALAF4_DIR_URL; ?>images/research-header-background-640.jpg"><img class="background show-for-medium" src="<?php echo ALAF4_DIR_URL; ?>images/research-header-background.jpg" srcset="<?php echo ALAF4_DIR_URL; ?>images/research-header-background-1024.jpg 1024w, <?php echo ALAF4_DIR_URL; ?>images/research-header-background-1200.jpg 1200w, <?php echo ALAF4_DIR_URL; ?>images/research-header-background-1440.jpg 1440w, <?php echo ALAF4_DIR_URL; ?>images/research-header-background.jpg 1900w" sizes="(max-width: 1024px) 1024px, (max-width: 1200px) 1200px, (max-width: 1440px) 1440px, 1900px">
-		<h1><img src="<?php echo ALAF4_DIR_URL; ?>images/research-header.png" alt="Texas A&M AgriLife Research"></h1>
+		<h1>Agriculture and Life Sciences</h1>
 	</div>
-	<div class="introduction layout-container"><p>Texas A&M AgriLife Research is the leading research and technology development agency in Texas. We conduct more than 500 agriculture, natural resources, and the life sciences projects each year supported with more than $206.9 million in research funding in 2016. We have a statewide presence and collaborate with scientists and research staff on other Texas A&M University System campuses and at the 13 regional Texas A&M AgriLife Research and research Centers. Our scientists span many disciplines uniting with other centers and entities improve in the following fields:</p></div>
+	<div class="introduction layout-container"><p>Agriculture and the life sciences has been an integral part of Texas A&M since 1876 when it was founded as the Agricultural and Mechanical College of Texas.  Naturally, with agriculture as one of the pillars on which Texas A&M University was established, the students we educate and the scientific advancements we discover are providing better nutrition and agriculture.</p><p>From long-established majors such as agronomy and animal science to newer programs such as forensics and spatial sciences, the College of Agriculture and Life Sciences is widely recognized as a leader in dozens of academic disciplines.</p></div>
 	<div class="flowchart brackets">
 		<div class="layout-container">
 			<div class="flowchart-row top">
 				<div class="al item">
-					<a href="https://agrilife.org/" title="Texas A&M AgriLife research"><span><img src="<?php echo AF_THEME_DIRURL; ?>/images/logo-tamu-agrilife-research.png" alt="Texas A&M AgriLife research"></span></a>
+					<a href="https://agrilife.org/" title="Texas A&M College of Agriculture and Life Sciences"><span><img src="<?php echo AF_THEME_DIRURL; ?>/images/logo-college.png" alt="Texas A&M College of Agriculture and Life Sciences"></span></a>
 				</div>
 			</div>
 			<div class="flowchart-row bottom"><div class="ext item"><p><span>Food &amp; Nutrition</span></p></div><div class="res item"><p><span>Disease Prevention</span></p></div><div class="college item"><p><span>Land Use</span></p></div><div class="tvmdl item"><p><span>Bioenergy</span></p></div><div class="tfs item"><p><span>Sustainability</span></p></div></div>
 			<div class="flowchart-row bottom"><div class="ext item"><p><span>Pests &amp; Invasive Plants</span></p></div><div class="res item"><p><span>Insect-Vectored Diseases</span></p></div><div class="college item"><p><span>New crops</span></p></div><div class="tvmdl item"><p><span>Water</span></p></div><div class="tfs item"><p><span>Livestock & Plant Genetics</span></p></div></div>
 		</div>
 	</div>
-	<div class="buttons"><a class="button" href="https://agriliferesearch.tamu.edu/">agriliferesearch.tamu.edu</a></div>
+	<div class="buttons"><a class="button" href="https://aglifesciences.tamu.edu/">aglifesciences.tamu.edu</a></div>
 	<div class="exceptional-items">
 		<div class="layout-container">
 			<h2>Exceptional Items</h2>
@@ -31,7 +31,7 @@ add_action('genesis_entry_content', function(){
 		</div>
 	</div>
 	<div class="director">
-		<div class="layout-container"><div class="text-column"><div class="quote">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ultricies vestibulum ornare. Morbi eget consequat augue. Pellentesque lacinia eget nibh ut dapibus. Mauris metus dui, pulvinar nec nibh id, suscipit aliquam dolor. </div><div class="creds">Dr. Parr Rosson, Interim Director
+		<div class="layout-container"><div class="text-column"><div class="quote">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ultricies vestibulum ornare. Morbi eget consequat augue. Pellentesque lacinia eget nibh ut dapibus. Mauris metus dui, pulvinar nec nibh id, suscipit aliquam dolor. </div><div class="creds">Patrick J. Stover, Vice Chancellor and Dean
 <p>Morbi eget consequat augue pellentesque<br>
 Mauris metus dui, pulvinar nec nibh id suscipit<br>
 Vestibulum nec interdum velit curabitur nec bibendum metus</p></div></div><div class="photo-column"><img src="<?php echo ALAF4_DIR_URL; ?>images/research-director.jpg" alt="Mosquito"></div></div>
--- templates/forest.php
+++ templates/forest.php
@@ -9,9 +9,9 @@ add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_c
 
 add_action('genesis_entry_content', function(){
 	?><div class="content-heading-image"><img class="background show-for-small-only" src="<?php echo ALAF4_DIR_URL; ?>images/research-header-background-640.jpg"><img class="background show-for-medium" src="<?php echo ALAF4_DIR_URL; ?>images/research-header-background.jpg" srcset="<?php echo ALAF4_DIR_URL; ?>images/research-header-background-1024.jpg 1024w, <?php echo ALAF4_DIR_URL; ?>images/research-header-background-1200.jpg 1200w, <?php echo ALAF4_DIR_URL; ?>images/research-header-background-1440.jpg 1440w, <?php echo ALAF4_DIR_URL; ?>images/research-header-background.jpg 1900w" sizes="(max-width: 1024px) 1024px, (max-width: 1200px) 1200px, (max-width: 1440px) 1440px, 1900px">
-		<h1><img src="<?php echo ALAF4_DIR_URL; ?>images/research-header.png" alt="Texas A&M AgriLife Research"></h1>
+		<h1>Forest Service</h1>
 	</div>
-	<div class="introduction layout-container"><p>Texas A&M AgriLife Research is the leading research and technology development agency in Texas. We conduct more than 500 agriculture, natural resources, and the life sciences projects each year supported with more than $206.9 million in research funding in 2016. We have a statewide presence and collaborate with scientists and research staff on other Texas A&M University System campuses and at the 13 regional Texas A&M AgriLife Research and research Centers. Our scientists span many disciplines uniting with other centers and entities improve in the following fields:</p></div>
+	<div class="introduction layout-container"><p>Texas A&M Forest Service (TFS) is the leader in forestry for Texas and the nation. TFS works to ensure the state’s forests, trees, and related natural resources are conserved and continue to provide a sustainable flow of environmental and economic benefits. Applied programs include:</p></div>
 	<div class="flowchart brackets">
 		<div class="layout-container">
 			<div class="flowchart-row top">
@@ -19,11 +19,14 @@ add_action('genesis_entry_content', function(){
 					<a href="https://agrilife.org/" title="Texas A&M AgriLife research"><span><img src="<?php echo AF_THEME_DIRURL; ?>/images/logo-tamu-agrilife-research.png" alt="Texas A&M AgriLife research"></span></a>
 				</div>
 			</div>
-			<div class="flowchart-row bottom"><div class="ext item"><p><span>Food &amp; Nutrition</span></p></div><div class="res item"><p><span>Disease Prevention</span></p></div><div class="college item"><p><span>Land Use</span></p></div><div class="tvmdl item"><p><span>Bioenergy</span></p></div><div class="tfs item"><p><span>Sustainability</span></p></div></div>
-			<div class="flowchart-row bottom"><div class="ext item"><p><span>Pests &amp; Invasive Plants</span></p></div><div class="res item"><p><span>Insect-Vectored Diseases</span></p></div><div class="college item"><p><span>New crops</span></p></div><div class="tvmdl item"><p><span>Water</span></p></div><div class="tfs item"><p><span>Livestock & Plant Genetics</span></p></div></div>
+			<div class="flowchart-row bottom"><div class="item"><p><span>Forest and tree development</span></p></div><div class="item"><p><span>Wildfire prevention</span></p></div><div class="item"><p><span>Mitigation and protection</span></p></div><div class="item"><p><span>Urban and community forestry</span></p></div><div class="item"><p><span>Forest sustainability programs</span></p></div></div>
+			<div class="flowchart-row top count-3">
+				<div class="al item h5">TFS is also the incident management agency for state disasters such as</div>
+			</div>
+			<div class="flowchart-row bottom count-3"><div class="item"><p><span>Wildfires</span></p></div><div class="item"><p><span>Flooding</span></p></div><div class="item"><p><span>Hurricanes</span></p></div></div>
 		</div>
 	</div>
-	<div class="buttons"><a class="button" href="https://agriliferesearch.tamu.edu/">agriliferesearch.tamu.edu</a></div>
+	<div class="buttons"><a class="button" href="https://texasforestservice.tamu.edu/">texasforestservice.tamu.edu</a></div>
 	<div class="exceptional-items">
 		<div class="layout-container">
 			<h2>Exceptional Items</h2>
