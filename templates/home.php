46c6bb833ea7b42bd3d9d3a44e6138789a8b7988
Travis Ward <travis@travsiward.com>
2018-12-17 17:15:28 -0600

    Add links on homepage template.

--- templates/home.php
+++ templates/home.php
@@ -15,20 +15,26 @@ add_action('genesis_entry_content', function(){
 		<div id="leaders-heading" class="layout-container"><h1><span class="heading-1">Leaders in </span><span class="heading-2">Agriculture,<br> Natural Resources,<br> &amp; Life Sciences</span></h1></div>
 		<div id="action-items" class="layout-container">
 			<div class="item item-1"><div class="wrap"><h2>86th Legislature</h2>
-				<div class="description has-line hide-for-small-only"><ul><li><span class="show-for-xlarge">Texas A&M AgriLife </span>Extension<span class="show-for-medium"> Service</span></li>
-				<li><span class="show-for-xlarge">Texas A&M AgriLife </span>Research</li>
-				<li><span class="show-for-xlarge">Texas A&M </span>Veterinary<span class="hide-for-medium hide-for-large"> Medical</span> Diagnostic<span class="hide-for-xlarge">s</span><span class="show-for-xlarge"> Laboratory</span></li>
-				<li><span class="show-for-xlarge">Texas A&M </span>Forest<span class="show-for-medium hide-for-large">ry</span><span class="show-for-xlarge"> Service</span></li></ul></div></div>
+				<div class="description has-line hide-for-small-only">
+					<ul>
+						<li><a href="/agency/extension-home/#ei"><span class="show-for-xlarge">Texas A&amp;M AgriLife </span>Extension<span class="show-for-medium"> Service</span></a></li>
+						<li><a href="/agency/research-home/#ei"><span class="show-for-xlarge">Texas A&amp;M AgriLife </span>Research</a></li>
+						<li><a href="/agency/tvmdl-home/#ei"><span class="show-for-xlarge">Texas A&amp;M </span>Veterinary<span class="hide-for-medium hide-for-large"> Medical</span> Diagnostic<span class="hide-for-xlarge">s</span><span class="show-for-xlarge"> Laboratory</span></a></li>
+						<li><a href="/agency/tfs-home/#ei"><span class="show-for-xlarge">Texas A&amp;M </span>Forest<span class="show-for-medium hide-for-large">ry</span><span class="show-for-xlarge"> Service</span></a></li>
+					</ul></div></div>
 			</div>
-			<div class="item item-2"><img class="hide-for-small-only" src="<?php echo ALAF4_DIR_URL; ?>/images/home-about.jpg" alt=""><h2>What is <br>Texas A&M AgriLife?</h2></div>
+			<div class="item item-2"><a href="/about/"><img class="hide-for-small-only" src="<?php echo ALAF4_DIR_URL; ?>/images/home-about.jpg" alt="About AgriLife"></a><h2><a href="/about/">What is <br>Texas A&amp;M AgriLife?</a></h2></div>
 			<div class="item  item-3 featured"><div class="wrap" style="background-image: url(<?php echo ALAF4_DIR_URL; ?>/images/home-impacts.jpg);"><img src="<?php echo ALAF4_DIR_URL; ?>/images/home-impacts.jpg" alt=""><h2>Impacts</h2>
 				<div class="description has-line hide-for-small-only">Residents, AgriLife Extension, others work to 'Harvey-proof' Houston-area community</div>
 			</div></div>
-			<div class="item item-4"><img class="hide-for-small-only" src="<?php echo ALAF4_DIR_URL; ?>/images/home-today.jpg" alt=""><h2>AgriLife Today</h2>
-				<div class="description has-line hide-for-small-only">Texas A&M AgriLife to lead consortium in establishing Center
+			<div class="item item-4"><a href="https://today.agrilife.org/2018/10/02/texas-am-agrilife-to-lead-consortium-in-establishing-center-for-cross-border-threat-screening-and-supply-chain-defense/"><img class="hide-for-small-only" src="<?php echo ALAF4_DIR_URL; ?>/images/home-today.jpg" alt="AgriLife Today"></a><h2><a href="https://today.agrilife.org/2018/10/02/texas-am-agrilife-to-lead-consortium-in-establishing-center-for-cross-border-threat-screening-and-supply-chain-defense/">AgriLife Today</a></h2>
+				<div class="description has-line hide-for-small-only">Texas A&amp;M AgriLife to lead consortium in establishing Center
 			</div></div>
 			<div class="item item-5">
-				<div class="wrap"><h2><span class="big">October Newsletter</span></h2><!-- Begin Mailchimp Signup Form -->
+				<div class="wrap">
+					<h2><span class="big">Newsletter</span></h2>
+					<h3><a href="#" class"latest-newsletter">November 2018</a></h3>
+					<!-- Begin Mailchimp Signup Form -->
 					<div id="mc_embed_signup" class="description">
 						<form action="https://agrilife.us1.list-manage.com/subscribe/post?u=1a8a0ed97f45319b1319f2572&amp;id=449a7707a3" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 					    <div id="mc_embed_signup_scroll">
@@ -52,7 +58,11 @@ add_action('genesis_entry_content', function(){
 	<div id="agencies" class="flowchart no-arrow">
 		<div class="layout-container">
 			<div class="flowchart-row top"><div class="al item"><a href="https://agrilife.org/" title="Texas A&M AgriLife"><span><img src="<?php echo AF_THEME_DIRURL; ?>/images/logo-tamu-agrilife.png" alt="Texas A&M AgriLife"></span></a></div></div>
-			<div class="flowchart-row bottom"><div class="ext item"><a href="http://agrilifeextension.tamu.edu/" title="Texas A&M AgriLife Extension Service"><span><img class="hide-for-small-only" src="<?php echo AF_THEME_DIRURL; ?>/images/logo-tamu-agrilife-extension.png" alt="Texas A&M AgriLife Extension Service"><span class="show-for-small-only">Texas A&amp;M AgriLife Extension</span></span></a></div><div class="res item"><a href="http://agriliferesearch.tamu.edu/" title="Texas A&M AgriLife Research"><span><img class="hide-for-small-only" src="<?php echo AF_THEME_DIRURL; ?>/images/logo-tamu-agrilife-research.png" alt="Texas A&M AgriLife Research"><span class="show-for-small-only">Texas A&amp;M AgriLife Research</span></span></a></div><div class="college item"><a href="http://aglifesciences.tamu.edu/" title="Texas A&M College of Agrculture and Life Sciences"><span><img class="hide-for-small-only" src="<?php echo AF_THEME_DIRURL; ?>/images/logo-tamu-coals-vertical-white-small.png" alt="Texas A&M College of Agrculture and Life Sciences"><span class="show-for-small-only">Texas A&amp;M University College of Agriculture &amp; Life Sciences</span></span></a></div><div class="tvmdl item"><a href="http://tvmdl.tamu.edu/" title="Texas A&M Veterinary Medical Diagnostics Laboratory"><span><img class="hide-for-small-only" src="<?php echo AF_THEME_DIRURL; ?>/images/logo-tamu-tvmdl.png" alt="Texas A&M Veterinary Medical Diagnostics Laboratory"><span class="show-for-small-only">Texas A&amp;M Veterinary Medical Diagnostic Laboratory</span></span></a></div><div class="tfs item"><a href="http://texasforestservice.tamu.edu/" title="Texas A&M Forest Service"><span><img class="hide-for-small-only" src="<?php echo AF_THEME_DIRURL; ?>/images/logo-tamu-forest-service.png" alt="Texas A&M Forest Service"><span class="show-for-small-only">Texas A&amp;M Forest Service</span></span></a></div></div>
+			<div class="flowchart-row bottom"><div class="ext item"><a href="/agency/extension-home/" title="Texas A&M AgriLife Extension Service"><span><img class="hide-for-small-only" src="<?php echo AF_THEME_DIRURL; ?>/images/logo-tamu-agrilife-extension.png" alt="Texas A&M AgriLife Extension Service"><span class="show-for-small-only">Texas A&amp;M AgriLife Extension</span></span></a></div>
+			<div class="res item"><a href="/agency/research-home/" title="Texas A&M AgriLife Research"><span><img class="hide-for-small-only" src="<?php echo AF_THEME_DIRURL; ?>/images/logo-tamu-agrilife-research.png" alt="Texas A&M AgriLife Research"><span class="show-for-small-only">Texas A&amp;M AgriLife Research</span></span></a></div>
+			<div class="college item"><a href="/agency/college-home/" title="Texas A&M College of Agrculture and Life Sciences"><span><img class="hide-for-small-only" src="<?php echo AF_THEME_DIRURL; ?>/images/logo-tamu-coals-vertical-white-small.png" alt="Texas A&M College of Agrculture and Life Sciences"><span class="show-for-small-only">Texas A&amp;M University College of Agriculture &amp; Life Sciences</span></span></a></div>
+			<div class="tvmdl item"><a href="/agency/tvmdl-home/" title="Texas A&M Veterinary Medical Diagnostics Laboratory"><span><img class="hide-for-small-only" src="<?php echo AF_THEME_DIRURL; ?>/images/logo-tamu-tvmdl.png" alt="Texas A&M Veterinary Medical Diagnostics Laboratory"><span class="show-for-small-only">Texas A&amp;M Veterinary Medical Diagnostic Laboratory</span></span></a></div>
+			<div class="tfs item"><a href="/agency/tfs-home/" title="Texas A&M Forest Service"><span><img class="hide-for-small-only" src="<?php echo AF_THEME_DIRURL; ?>/images/logo-tamu-forest-service.png" alt="Texas A&M Forest Service"><span class="show-for-small-only">Texas A&amp;M Forest Service</span></span></a></div></div>
 		</div>
 	</div>
 	<?php
