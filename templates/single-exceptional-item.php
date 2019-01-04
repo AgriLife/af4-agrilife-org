029cd8c5cae5fa76feb3226e9c249e536c6e8367
Travis Ward <travis@travsiward.com>
2019-01-04 08:54:51 -0600

    Update language in exceptional item

--- templates/single-exceptional-item.php
+++ templates/single-exceptional-item.php
@@ -34,7 +34,7 @@ function af4_ei_right_column() {
 					$request_fields = get_field('request');
 
 				?>
-				<h3>Exceptional Item Requisition</h3>
+				<h3>Exceptional Item Request</h3>
 				<h3>FY <?php echo $request_fields['year']; ?></h3>
 					<div class="value"><?php echo $request_fields['amount']; ?></div>
 				</div>
