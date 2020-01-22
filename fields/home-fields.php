1fe9ebc6ba06b4468d607b2daae54049a019be11
Zachary Watkins <watkinza@gmail.com>
2020-01-22 15:13:26 -0600

    Add A/B testing custom field to home page template

--- fields/home-fields.php
+++ fields/home-fields.php
@@ -493,6 +493,33 @@ if ( function_exists( 'acf_add_local_field_group' ) ) :
 						),
 					),
 				),
+				array(
+					'key'               => 'field_5e28a01f74ace',
+					'label'             => 'A/B Test',
+					'name'              => 'ab_test',
+					'type'              => 'select',
+					'instructions'      => '',
+					'required'          => 0,
+					'conditional_logic' => 0,
+					'wrapper'           => array(
+						'width' => '',
+						'class' => '',
+						'id'    => '',
+					),
+					'choices'           => array(
+						'Option 1' => 'Option 1',
+						'Option 2' => 'Option 2',
+					),
+					'default_value'     => array(
+						0 => 'Option 1',
+					),
+					'allow_null'        => 0,
+					'multiple'          => 0,
+					'ui'                => 0,
+					'return_format'     => 'value',
+					'ajax'              => 0,
+					'placeholder'       => '',
+				),
 			),
 			'location'              => array(
 				array(
